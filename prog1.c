86bd0db0d6f9604407b274842fe84f26fdac3921
Cole Petersen <peterco4@flip2.engr.oregonstate.edu>
2026-01-03 09:26:04 -0800

    Completed work

--- prog1.c
+++ prog1.c
@@ -1,5 +1,6 @@
 #include <stdio.h> // Necessary for printf()
-
+#include <stdlib.h>
+#include <time.h>
 // TODO Other includes
 
 
@@ -8,4 +9,18 @@ int main() {
 	// is executed, it will automatically start here.
 	
 	// TODO Complete the program
+	
+
+	srand(time(NULL)); // resets the seed
+	int myrand = rand() % 11; // random number from 0 to 10
+
+	// If statements
+	if (myrand == 10) {
+		printf("Larn is the best roguelike\n");
+	} else if (myrand <= 4) {
+		printf("Eat more beef, kick less cats\n");
+	} else if (myrand >= 4 && myrand <= 9) {
+		printf("FRODO LIVES\n");
+	}
+	
 }
